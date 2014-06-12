9824021115cbf1e35490f5d34e87c0cfcdcceaae
Jenner Felton <jfelton@g.hmc.edu>
2014-06-12 15:14:00 -0700

    removed old migrates

--- db/migrate/20140611164600_add_order_to_line_item.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderToLineItem < ActiveRecord::Migration
-  def change
-    add_reference :line_items, :order, index: true
-  end
-end
--- db/migrate/20140611225848_add_ship_date_to_order.rb
+++
@@ -1,9 +0,0 @@
-class AddShipDateToOrder < ActiveRecord::Migration
-  def self.up
-  	add_column :orders, :ship_date, :datetime
-  end
-
-  def self.down
-  	remove_column :orders, :ship_date
-  end
-end
